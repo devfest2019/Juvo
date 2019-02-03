d0457eee476f6d840559a49bfe9bc8985b288535
samuelfu <sfu2000@gmail.com>
2019-02-03 01:13:07 -0500

    Added model saver and loader API

---
+++ model_helper.py
@@ -0,0 +1,29 @@
+from keras.models import model_from_json
+
+# coding: utf-8
+
+# In[ ]:
+
+def save_model(model):
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("model.h5")
+    print("Saved model to disk")
+
+    # later...
+def load_model():
+    # load json and create model
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
+    print("Loaded model from disk")
+
+    # evaluate loaded model on test data
+    loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    return loaded_model
