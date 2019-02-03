6fa5814964a20387489f09473f327ad0ef92e752
1jinwoo <justinwon97@gmail.com>
2019-02-03 01:29:53 -0500

    Add model_api.py

---
+++ model_api.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  3 01:26:59 2019
+
+@author: Justin Won
+"""
+
+def determine_acceptance(input):
+    '''
+    Take input form data and return binary decision.
+    '''
+    pass
